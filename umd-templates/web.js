d5d33ce8fed98b65c11d40bfff995e10a645c15f
Matthew Lee <math.lee@qq.com>
2017-09-27 00:21:18 +0800

    replace the ev by Event instance.

--- umd-templates/web.js
+++ umd-templates/web.js
@@ -8,10 +8,7 @@ if (!this.<%= namespace %>) (function(doc, ev) {
   ev = doc.createEvent('HTMLEvents');
   
   // the initEvent may be deprecated in the future, instead we can use the Event constructor
-  ev.initEvent ? ev.initEvent('sentinel-load', false, false) : new Event('sentinel-load', {
-    'bubbles': false, 
-    'cancelable': false 
-  });
+  ev.initEvent ? ev.initEvent('sentinel-load', false, false) : (ev = new Event('sentinel-load'));
   
   doc.dispatchEvent(ev);
 })(document);
